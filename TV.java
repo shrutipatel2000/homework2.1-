1ee9e1f9696643b28317e7882c85e59d36330164
shrutipatel2000 <149239594+shrutipatel2000@users.noreply.github.com>
2023-11-19 20:55:04 +0000

    Add files via upload

---
+++ TV.java
@@ -0,0 +1,29 @@
+package InheritancePackage;
+
+import java.sql.SQLOutput;
+
+/*
+switch on
+use remote
+start chanel
+watch tv
+switch off
+ */
+public class TV {
+
+    void switchon(){
+        System.out.println("TV.................switch on");
+    }
+    void useremote(){
+        System.out.println("TV...............use remote");
+    }
+    void startchanel(){
+        System.out.println("TV...............start chanel");
+    }
+    void watchtv(){
+        System.out.println("TV............watch tv");
+    }
+    void switchoff(){
+        System.out.println("TV..............switch off");
+    }
+}
